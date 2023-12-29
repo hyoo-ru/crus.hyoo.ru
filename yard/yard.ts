9b0650b3e40b9ebc52e4bdc936cae7f8c08b247d
jin <jin@hyoo.ru>
2023-12-29 18:13:27 +0300

    fixed node build

--- yard/yard.ts
+++ yard/yard.ts
@@ -1,4 +1,11 @@
 namespace $ {
 	export class $hyoo_crus_yard extends $mol_object {
+		
+		static load( land_ref: string ) {
+			return [] as readonly $hyoo_crus_unit[]
+		}
+		
+		static async save( land_ref: string, units: readonly $hyoo_crus_unit[] ) {}
+		
 	}
 }
