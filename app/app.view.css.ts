7cef8d38d659ba6e59e90340b3a5d4b03d7d59c0
jin <jin@hyoo.ru>
2024-01-01 02:38:08 +0300

    + bg

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,11 +1,21 @@
 namespace $.$$ {
 	
+	const { url, hsla, linear_gradient } = $mol_style_func
+	
 	$mol_style_define( $hyoo_crus_app, {
 		
 		Casting: {
 			flex: {
 				grow: 1,
 			},
+		},
+		
+		background: {
+			image: [
+				[ linear_gradient([ 'to right', [ hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, .5 ) ] ]) ],
+				[ url( 'https://i.imgur.com/oPsM5Ye.jpeg' ) ],
+			],
+			size: [ 'cover' ],
 		},
 		
 	} )
