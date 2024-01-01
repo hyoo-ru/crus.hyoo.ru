19b2866e9327de7a3d7b304dfde03b9f1da5d427
jin <jin@hyoo.ru>
2024-01-01 19:50:55 +0300

    +2

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -4,18 +4,22 @@ namespace $.$$ {
 	
 	$mol_style_define( $hyoo_crus_app, {
 		
+		background: {
+			image: [
+				[ linear_gradient([ 'to right', [ hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, .5 ) ] ]) ],
+				[ url( 'https://i.imgur.com/oPsM5Ye.jpeg' ) ],
+			],
+			size: [ 'cover' ],
+		},
+		
+		Intro: {
+			margin: [ 0, 'auto' ],
+		},
+		
 		Casting: {
 			flex: {
 				grow: 1,
 			},
-		},
-		
-		background: {
-			image: [
-				[ linear_gradient([ 'to right', [ hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, .5 ) ] ]) ],
-				[ url( 'https://i.imgur.com/oPsM5Ye.jpeg' ) ],
-			],
-			size: [ 'cover' ],
 		},
 		
 	} )
