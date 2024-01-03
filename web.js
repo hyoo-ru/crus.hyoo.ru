66899ba517fac186af51f44b1e0e55bd37de821a
nin-jin <sairi-na-tenshi@ya.ru>
2024-01-03 16:52:46 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@19201a1ad0b800ab85efe3b8f2ad8f0f95c73e8c 🚀

--- web.js
+++ web.js
@@ -20074,13 +20074,12 @@ var $;
 (function ($) {
     var $$;
     (function ($$) {
-        const { url, hsla, linear_gradient } = $mol_style_func;
+        const { url } = $mol_style_func;
         $mol_style_define($hyoo_crus_app, {
             background: {
-                image: [
-                    [url('hyoo/crus/logo/bg.webp')],
-                ],
+                image: [[url('hyoo/crus/logo/bg.webp')]],
                 size: ['cover'],
+                position: 'bottom',
             },
             Menu: {
                 flex: {
