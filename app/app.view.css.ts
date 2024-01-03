19201a1ad0b800ab85efe3b8f2ad8f0f95c73e8c
jin <jin@hyoo.ru>
2024-01-03 19:51:31 +0300

    +1

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -1,15 +1,13 @@
 namespace $.$$ {
 	
-	const { url, hsla, linear_gradient } = $mol_style_func
+	const { url } = $mol_style_func
 	
 	$mol_style_define( $hyoo_crus_app, {
 		
 		background: {
-			image: [
-				// [ linear_gradient([ 'to right', [ hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, 1 ), hsla( 0, 0, 0, .5 ) ] ]) ],
-				[ url( 'hyoo/crus/logo/bg.webp' ) ],
-			],
+			image: [[ url( 'hyoo/crus/logo/bg.webp' ) ]],
 			size: [ 'cover' ],
+			position: 'bottom',
 		},
 		
 		Menu: {
