069e087529a5000174e2ab9a7da1f8c701f0f486
jin <jin@hyoo.ru>
2024-03-23 16:52:46 +0300

    rooms -> hall

--- home/home.test.ts
+++ home/home.test.ts
@@ -5,11 +5,9 @@ namespace $ {
 			
 			const realm = $hyoo_crus_realm.make({ $ })
 			const base = realm.home()
-			const profile1 = base.room( 'my_foo', $hyoo_crus_dict, $hyoo_crus_rank_public )!
-			const profile2 = base.room( 'my_bar', $hyoo_crus_dict, $hyoo_crus_rank_public )!
+			const hall = base.hall_by( $hyoo_crus_dict, $hyoo_crus_rank_public )!
 			
-			$mol_assert_unique( base.land(), profile1, profile2 )
-			$mol_assert_equal( base.Rooms?.keys(), [ 'my_bar', 'my_foo' ] )
+			$mol_assert_unique( base.land(), hall )
 			
 		},
 
--- home/home.ts
+++ home/home.ts
@@ -2,15 +2,14 @@ namespace $ {
 	export class $hyoo_crus_home extends $hyoo_crus_dict.with({
 		Title: $hyoo_crus_atom_str,
 		Selection: $hyoo_crus_atom_str,
-		Rooms: $hyoo_crus_dict_to( $hyoo_crus_atom_ref_to( ()=> $hyoo_crus_dict ) ),
+		Hall: $hyoo_crus_atom_ref_to( ()=> $hyoo_crus_dict ),
 	}) {
 		
-		room< Node extends typeof $hyoo_crus_node >(
-			app: string,
+		hall_by< Node extends typeof $hyoo_crus_dict >(
 			Node: Node,
 			preset?: $hyoo_crus_rank_preset
 		) {
-			return this.Rooms?.key( app, null )?.remote_ensure( preset )?.cast( Node ) ?? null
+			return this.Hall?.remote_ensure( preset )?.cast( Node ) ?? null
 		}
 		
 	}
