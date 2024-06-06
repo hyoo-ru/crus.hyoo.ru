d3e5c449f20cd1251d9748cdff46fc5150e8f44f
jin <jin@hyoo.ru>
2024-06-06 08:46:02 +0300

    log db host fix

--- mine/pg/pg.node.ts
+++ mine/pg/pg.node.ts
@@ -128,8 +128,8 @@ namespace $ {
 				place: this,
 				message: 'Data Base Ready',
 				type: this.url().protocol,
-				origin: this.url().origin,
-				database: this.url().pathname,
+				host: this.url().host,
+				name: this.url().pathname,
 			})
 			
 			return db
