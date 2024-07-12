80fd701eeb99db7ba5937ee05969535aeba5fb9e
jin <jin@hyoo.ru>
2024-07-12 16:47:37 +0300

    +2

--- app/app.web.view.ts
+++ app/app.web.view.ts
@@ -2,15 +2,8 @@ namespace $.$$ {
 	export class $hyoo_crus_app extends $.$hyoo_crus_app {
 		
 		@ $mol_mem
-		realm() {
-			const realm = super.realm()
-			realm.home()
-			return realm
-		}
-		
-		@ $mol_mem
-		intro() {
-			return this.$.$mol_fetch.text( 'hyoo/crus/readme.md' )
+		auto() {
+			this.$.$hyoo_crus_realm.home()
 		}
 		
 	}
