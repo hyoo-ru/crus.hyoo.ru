4a9528fe446ba622739a2fe545a9fa9cbbde8eae
jin <jin@hyoo.ru>
2024-08-12 19:40:22 +0300

    less rebuffering

--- land/land.ts
+++ land/land.ts
@@ -862,7 +862,7 @@ namespace $ {
 			const sens = unit.sens().slice()
 			for( let i = 0; i < mixin.length; ++i ) sens[i+14] ^= mixin[i+14]
 			
-			const sign = new Uint8Array( key.sign( sens ) )
+			const sign = key.sign( sens )
 			unit.sign( sign )
 			
 		}
@@ -876,7 +876,7 @@ namespace $ {
 			let bin = sand._open
 			const secret = sand._land!.secret()!
 			
-			if( secret ) bin = new Uint8Array( $mol_wire_sync( secret ).encrypt( bin, sand.salt() ) )
+			if( secret ) bin = $mol_wire_sync( secret ).encrypt( bin, sand.salt() )
 			
 			if( bin.byteLength > 32 ) sand.hash( this.$.$hyoo_crus_mine.rock_save( bin ), sand.tip(), sand.tag() )
 			else sand.data( bin, sand.tip(), sand.tag() )
@@ -910,7 +910,7 @@ namespace $ {
 			let bin = sand.size() > 32 ? this.$.$hyoo_crus_mine.rock( sand.hash() ) : sand.data()
 			if( secret && bin && sand.tip() !== 'nil' ) {
 				try {
-					bin = new Uint8Array( $mol_wire_sync( secret ).decrypt( bin, sand.salt() ) )
+					bin = $mol_wire_sync( secret ).decrypt( bin, sand.salt() )
 				} catch( error: any ) {
 					if( $mol_fail_catch( error ) ) {
 						if( error.message ) $mol_fail_hidden( error )
@@ -973,7 +973,7 @@ namespace $ {
 			unit._land = this
 			
 			const secret_closed = $mol_wire_sync( secret_mutual ).encrypt( secret_land, unit.salt() )
-			unit.bill().set( new Uint8Array( secret_closed ) )
+			unit.bill().set( secret_closed )
 			
 			const error = this.apply_unit([ unit ])[0]
 			if( error ) $mol_fail( new Error( error ) )
