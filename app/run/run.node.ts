08159bf418ddec433757287b808593b41ed21ab5
jin <jin@hyoo.ru>
2024-08-16 11:52:01 +0300

    +1

--- app/run/run.node.ts
+++ app/run/run.node.ts
@@ -1,4 +1,4 @@
 namespace $ {
-s	$hyoo_crus_yard.masters = $mol_state_arg.value( 'masters' )?.split( ',' ) ?? []
+	$hyoo_crus_yard.masters = $mol_state_arg.value( 'masters' )?.split( ',' ) ?? []
 	$hyoo_crus_app.serve()
 }
