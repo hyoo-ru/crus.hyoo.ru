a850f51c332451e2c7fc2de660d6fae92bd15787
nin-jin <nin-jin@users.noreply.github.com>
2024-12-08 07:16:12 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@43d993840ce35733820e71aa76dd7357bbea32fb 🚀

--- web.mjs
+++ web.mjs
@@ -18000,7 +18000,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
