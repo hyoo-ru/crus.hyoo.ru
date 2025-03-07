0d7448c46fe5fd4a103b8e7ace63369ea51c9e50
nin-jin <sairi-na-tenshi@ya.ru>
2025-03-07 12:38:13 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@2cbbcc128ee7bd4274415ca8e67e6567acdc9e71 🚀

--- web.js
+++ web.js
@@ -8453,6 +8453,7 @@ var $;
         'https://crus.hyoo.ru/',
         'https://i-love-crus.ru/',
         'https://crus.xedth.ru/',
+        'https://crus.87-120-36-150.ip.hyoo.ru/',
     ];
 })($ || ($ = {}));
 
