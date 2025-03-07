40cb7f8f6525024df2609b0f3ee085b6f8ef3bf0
novgorodcev.k <kirrallt@mail.ru>
2025-03-07 14:24:10 +0300

    add

--- yard/yard.web.ts
+++ yard/yard.web.ts
@@ -1,8 +1,8 @@
 namespace $ {
-  $.$hyoo_crus_yard.masters = [
-    "https://crus.hyoo.ru/",
-    "https://i-love-crus.ru/",
-    "https://crus.xedth.ru/",
-    "https://crus.87-120-36-150.ip.hyoo.ru/",
-  ];
+	$.$hyoo_crus_yard.masters = [
+		'https://crus.hyoo.ru/',
+		'https://i-love-crus.ru/',
+		'https://crus.xedth.ru/',
+		'https://crus.87-120-36-150.ip.hyoo.ru/,
+	]
 }
