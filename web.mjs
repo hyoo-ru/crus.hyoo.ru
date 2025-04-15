d38489033ce176bcb6890984464bff10aa9f19d0
nin-jin <nin-jin@users.noreply.github.com>
2025-04-15 07:19:37 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@ad7960544a5074c33079c124d472bd2891dc3080 🚀

--- web.mjs
+++ web.mjs
@@ -13393,7 +13393,7 @@ var $;
             }
             menu_links() {
                 return this.spread_ids_filtered()
-                    .map(spread => this.Menu_link(spread));
+                    .map(spread => this.Menu_item(spread));
             }
             spread_ids_filtered() {
                 return this.spread_ids()
@@ -13468,6 +13468,18 @@ var $;
                     grow: 0,
                 },
                 alignSelf: 'stretch',
+            },
+            Menu_item: {
+                align: {
+                    items: 'flex-start',
+                },
+            },
+            Menu_link: {
+                flex: {
+                    grow: 1,
+                    shrink: 1,
+                    wrap: 'wrap',
+                },
             },
         });
     })($$ = $.$$ || ($.$$ = {}));
