83a3cf07ef6fe72443fe31f64a54615ea234861c
nin-jin <nin-jin@users.noreply.github.com>
2025-06-26 07:19:54 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@62a8e26eb98e0dafbfc77a80b1b7d9aa9e628cfe 🚀

--- web.js
+++ web.js
@@ -14191,8 +14191,14 @@ var $;
 
 ;
 	($.$mol_theme_auto) = class $mol_theme_auto extends ($.$mol_plugin) {
+		dark(){
+			return "$mol_theme_dark";
+		}
 		theme(){
-			return "";
+			return (this.dark());
+		}
+		light(){
+			return "$mol_theme_light";
 		}
 		attr(){
 			return {"mol_theme": (this.theme())};
@@ -14260,7 +14266,7 @@ var $;
     (function ($$) {
         class $mol_theme_auto extends $.$mol_theme_auto {
             theme() {
-                return this.$.$mol_lights() ? '$mol_theme_light' : '$mol_theme_dark';
+                return this.$.$mol_lights() ? this.light() : this.dark();
             }
         }
         $$.$mol_theme_auto = $mol_theme_auto;
