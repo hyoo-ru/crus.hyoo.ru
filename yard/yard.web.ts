6b14fbab41e80d929dfd553849db3473893841c3
nin-jin <sairi-na-tenshi@ya.ru>
2025-09-05 16:05:44 +0300

    Update yard.web.ts

--- yard/yard.web.ts
+++ yard/yard.web.ts
@@ -4,5 +4,6 @@ namespace $ {
 		'https://i-love-crus.ru/', // amigun
 		//'https://crus.xedth.ru/', zubkov_p, unsupported
 		'https://crus.87-120-36-150.ip.hyoo.ru/', // Dev_Cmyser
+		'https://crus1.ocas.ai/', // webmalex
 	]
 }
