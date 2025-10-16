e4715bf01fbae8d22dd6db645642bc4a8eacf91e
nin-jin <nin-jin@users.noreply.github.com>
2025-10-16 07:18:50 +0000

    Deploying to gh-pages from @ hyoo-ru/crus.hyoo.ru@7bc3d4b24383d4782fe3ad9be8c9ff1eeb699e08 🚀

--- web.d.ts
+++ web.d.ts
@@ -285,6 +285,7 @@ declare namespace $ {
         toString(): string;
         toJSON(): string;
         [$mol_dev_format_head](): any[];
+        [$mol_dev_format_body](): null;
         get $(): any;
         emit(quant?: $mol_wire_cursor): void;
         fresh(): this | undefined;
@@ -8441,6 +8442,7 @@ declare namespace $ {
 
 	export class $mol_image extends $mol_view {
 		uri( ): string
+		title( ): string
 		loading( ): string
 		decoding( ): string
 		cors( ): any
