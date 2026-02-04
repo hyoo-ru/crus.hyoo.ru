a14a88bfd88af50605b1deba58d27d1ab027db2b
nin-jin <sairi-na-tenshi@ya.ru>
2026-02-05 00:19:34 +0300

    Update yard.web.ts

--- yard/yard.web.ts
+++ yard/yard.web.ts
@@ -1,6 +1,6 @@
 namespace $ {
 	$.$hyoo_crus_yard.masters = [
-		'https://crus.hd4.ru/', // main
+		//'https://crus.hd4.ru/', // switched to giper baza
 		'https://i-love-crus.ru/', // amigun
 		//'https://crus.xedth.ru/', zubkov_p, unsupported
 		'https://crus.87-120-36-150.ip.hyoo.ru/', // Dev_Cmyser
